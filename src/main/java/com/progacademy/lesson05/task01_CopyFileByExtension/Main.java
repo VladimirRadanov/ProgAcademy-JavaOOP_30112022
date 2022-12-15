c3bdb0747a03287a18eedbe8a08864194aaba4e8
Vladimir Radanov <vladimir.radanov@gmail.com>
2022-12-15 16:33:30 +0200

    lesson05 - task 01 - CopyFileByExtension(mkdir fix)

--- src/main/java/com/progacademy/lesson05/task01_CopyFileByExtension/Main.java
+++ src/main/java/com/progacademy/lesson05/task01_CopyFileByExtension/Main.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        File inputDir = new File("src/main/java/com/progacademy/lesson05/task01_CopyFileByExtention/inputDir");
-        File outputDir = new File("src/main/java/com/progacademy/lesson05/task01_CopyFileByExtention/outputDir");
+        File inputDir = new File("src/main/java/com/progacademy/lesson05/task01_CopyFileByExtension/inputDir/");
+        File outputDir = new File("src/main/java/com/progacademy/lesson05/task01_CopyFileByExtension/outputDir/");
+
+        outputDir.mkdir();
 
         try {
             System.out.println("Number of files copied: " +
